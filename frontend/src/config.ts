b75125444fc0e74e68f307b23288e8de9ca8ebb0
Jonatas Tavares <Jonatas.Tavares@NOTE-CM-07.metropoles.local>
2025-08-19 11:55:34 -0300

    config de deploy

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? "http://127.0.0.1:8000";
+export const API_URL = import.meta.env.VITE_API_URL ?? "https://sistema-busca-de-pi.onrender.com";
 export const DEFAULT_LIMIT = 20;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sistema_busca_de_pi/',
 })
